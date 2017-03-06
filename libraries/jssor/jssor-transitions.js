1189c9b89944649a834e89d976ca2ebd2d0e4a9b
icarus12345 <khuongxuantruong@gmail.com>
2017-03-06 21:57:10 +0700

    creative transition

--- libraries/jssor/jssor-transitions.js
+++ libraries/jssor/jssor-transitions.js
@@ -1,9 +1,11 @@
 var jssorTransitions = function(w,h){
-    var cols = 8; rows = Math.round(h/(w/8));
-    if(rows>8){
-        rows = 8;
-        cols = Math.round(w/(h/8));
+    var maxcols = Math.min(8,Math.floor(Math.max(w,h)/100));
+    var cols = maxcols; rows = Math.round(h/(w/maxcols));
+    if(rows>maxcols){
+        rows = maxcols;
+        cols = Math.round(w/(h/maxcols));
     }
+    console.log(maxcols,cols,rows)
     return [
         {$Duration:1200,x:0.2,y:-0.1,$Delay:20,$Cols:cols,$Rows:rows,$Clip:15,$During:{$Left:[0.3,0.7],$Top:[0.3,0.7]},$Formation:$JssorSlideshowFormations$.$FormationStraightStairs,$Easing:{$Left:$JssorEasing$.$EaseInWave,$Top:$JssorEasing$.$EaseInWave,$Clip:$JssorEasing$.$EaseOutQuad},$Assembly:260,$Outside:true,$Round:{$Left:1.3,$Top:2.5}},
         {$Duration:1200,x:0.2,y:-0.1,$Delay:20,$Cols:cols,$Rows:rows,$Clip:15,$During:{$Left:[0.3,0.7],$Top:[0.3,0.7]},$Formation:$JssorSlideshowFormations$.$FormationZigZag,$Easing:{$Left:$JssorEasing$.$EaseInWave,$Top:$JssorEasing$.$EaseInWave,$Clip:$JssorEasing$.$EaseOutQuad},$Assembly:260,$Outside:true,$Round:{$Left:1.3,$Top:2.5}},
@@ -46,7 +48,7 @@ var jssorTransitions = function(w,h){
         {$Duration:800,$Delay:300,$Cols:cols,$Rows:rows,$Clip:15,$Formation:$JssorSlideshowFormations$.$FormationSquare,$Easing:$JssorEasing$.$EaseInQuad},
         {$Duration:800,$Delay:300,$Cols:cols,$Rows:rows,$Clip:15,$Formation:$JssorSlideshowFormations$.$FormationRectangleCross,$Easing:$JssorEasing$.$EaseInQuad},
         {$Duration:600,x:1,y:-1,$Delay:50,$Cols:cols,$Rows:rows,$SlideOut:true,$ChessMode:{$Column:3,$Row:12},$Easing:{$Left:$JssorEasing$.$EaseInCubic,$Top:$JssorEasing$.$EaseInCubic,$Opacity:$JssorEasing$.$EaseOutQuad},$Opacity:2},
-        {$Duration:600,x:-1,y:1,$Delay:50,$Cols:8,$Rows:4,$ChessMode:{$Column:3,$Row:12},$Easing:{$Left:$JssorEasing$.$EaseInCubic,$Top:$JssorEasing$.$EaseInCubic,$Opacity:$JssorEasing$.$EaseOutQuad},$Opacity:2},
+        {$Duration:600,x:-1,y:1,$Delay:50,$Cols:cols,$Rows:rows,$ChessMode:{$Column:3,$Row:12},$Easing:{$Left:$JssorEasing$.$EaseInCubic,$Top:$JssorEasing$.$EaseInCubic,$Opacity:$JssorEasing$.$EaseOutQuad},$Opacity:2},
 
         {$Duration:700,$Opacity:2,$Brother:{$Duration:1000,$Opacity:2}},
         {$Duration:1500,x:0.5,$Cols:2,$ChessMode:{$Column:3},$Easing:{$Left:$JssorEasing$.$EaseInOutCubic},$Opacity:2,$Brother:{$Duration:1500,$Opacity:2}},
