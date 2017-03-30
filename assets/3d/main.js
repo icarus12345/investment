dc35f9457d4a7dae6e549c41f181313fdfd10860
icarus12345 <khuongxuantruong@gmail.com>
2017-03-30 22:54:27 +0700

    3d

--- assets/3d/main.js
+++ assets/3d/main.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $('.service-list>div>div').click(function(){
         var index = $(this).parent().index();
         console.log(index)
+
         $(this).parents('.service-list').find('.active').removeClass('active');
         $(this).parent().addClass('active');
         $(this).parents('.service-list').next().find('.active').removeClass('active');
@@ -14,4 +15,12 @@ $(document).ready(function(){
 function gotopage(p){
     $('body').removeClass('in')
     $('.pagination>.point:eq('+p+')').click()
+}
+function showitem(n){
+    // $('.slice-slider-slide-right .interes>.service-item:eq('+n+')>div').click();
+    $('.slice-slider-slide.active .slice-slider-slide-right .service-list').find('.active').removeClass('active');
+    $('.slice-slider-slide.active .slice-slider-slide-right .service-list').find('>.service-item:eq('+n+')').addClass('active');
+    $('.slice-slider-slide.active .slice-slider-slide-right .service-list').next().find('.active').removeClass('active');
+    $('.slice-slider-slide.active .slice-slider-slide-right .service-list').next().find('>div:eq(' + n + ')').addClass('active');
+    console.log(n)
 }
